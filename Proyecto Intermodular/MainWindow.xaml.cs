8746bcb3fc51f06b631256190f86dc9308f59263
Alvaro Martinez <alvaromagu2002@gmail.com>
2021-12-21 10:38:56 +0100

    Drag and Drop Mesas

--- Proyecto Intermodular/MainWindow.xaml.cs
+++ Proyecto Intermodular/MainWindow.xaml.cs
@@ -1,28 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Proyecto_Intermodular
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        bool distribution = true;
+        int borderSize = 25;
+        List<Table> tables;
+
         public MainWindow()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
+
+            tables = new();
+            tables.Add(new Table(1, 10, 10));
+            tables.Add(new Table(3, 40, 40));
+
+            tables.ForEach(table => AddTable(table));
+        }
+
+        private void AddTable(Table table)
+        {
+            Border border = new();
+            border.Background = (SolidColorBrush) new BrushConverter().ConvertFromString("#FF7AA0CD");
+            border.CornerRadius = new(10);
+            border.Width = borderSize;
+            border.Height = borderSize;
+            border.Name = $"brdTable{table.Id}";
+            Canvas.SetLeft(border, table.PosX);
+            Canvas.SetTop(border, table.PosY);
+
+            border.MouseMove += new MouseEventHandler((object sender, MouseEventArgs e) => {
+
+                if (e.LeftButton != MouseButtonState.Pressed) return;
+
+                if (distribution) DragDrop.DoDragDrop(border, new DataObject(DataFormats.Serializable, border), DragDropEffects.Move);
+                else MessageBox.Show("New ticket.");
+            });
+
+            Label label = new();
+            label.Content = table.Id;
+            label.HorizontalAlignment = HorizontalAlignment.Center;
+            label.VerticalAlignment = VerticalAlignment.Center;
+
+            border.Child = label;
+
+            cnvTables.Children.Add(border);
+            table.Border = border;
+        }
+
+        private void CnvTables_Drop(object sender, DragEventArgs e)
+        {
+            object data = e.Data.GetData(DataFormats.Serializable);
+            if(data is UIElement element)
+            {
+                Point dropPos = e.GetPosition(cnvTables);
+                double offset = borderSize / 2;
+
+                double left = (dropPos.X > cnvTables.ActualWidth - borderSize) ? cnvTables.ActualWidth - borderSize : 
+                              (dropPos.X < borderSize / 2) ? 0 : dropPos.X - offset;
+
+                double top = (dropPos.Y > cnvTables.ActualHeight - borderSize) ? cnvTables.ActualHeight - borderSize :
+                             (dropPos.Y < borderSize / 2) ? 0 : dropPos.Y - offset;
+
+                Canvas.SetLeft(element, left);
+                Canvas.SetTop(element, top);
+            }
+        }
+
+        private void BtnDistribution_Click(object sender, RoutedEventArgs e) => distribution = !distribution;
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Size oldSize = e.PreviousSize;
+            Size newSize = e.NewSize;
+
+            foreach(Border border in cnvTables.Children)
+            {
+                Point oldPoint = new(Canvas.GetLeft(border), Canvas.GetTop(border));
+                Point newPoint = new(oldPoint.X * newSize.Width / oldSize.Width, oldPoint.Y * newSize.Height / oldSize.Height);
+
+                if (newPoint.X + borderSize > cnvTables.ActualWidth) 
+                    newPoint.X = cnvTables.ActualWidth - borderSize;
+                if (newPoint.Y + borderSize > cnvTables.ActualHeight) 
+                    newPoint.Y = cnvTables.ActualHeight - borderSize;
+
+                Canvas.SetLeft(border, newPoint.X);
+                Canvas.SetTop(border, newPoint.Y);
+            }
         }
     }
 }
---
+++ Proyecto Intermodular/Table.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Proyecto_Intermodular
+{
+    class Table
+    {
+
+        private int id;
+        private double posXPercent;
+        private double posYPercent;
+        private double posX;
+        private double posY;
+        private Border border;
+
+        public Table(int id, double posXPercent, double posYPercent)
+        {
+            this.id = id;
+            this.posXPercent = posXPercent;
+            this.posYPercent = posYPercent;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Border Border { get => border; set => border = value; }
+
+        private void UpdatePosition(double newWidth, double newHeight)
+        {
+            posX = posXPercent * newWidth;
+            posY = posYPercent * newHeight;
+        }
+
+    }
+}
