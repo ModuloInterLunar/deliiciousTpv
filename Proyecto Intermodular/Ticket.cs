6c51e320a627593fedbf6c65200bfa79c61a9416
Alvaro Martinez <alvaromagu2002@gmail.com>
2022-01-05 13:29:08 +0100

    Ticket orders (add and remove)

--- Proyecto Intermodular/Ticket.cs
+++ Proyecto Intermodular/Ticket.cs
@@ -12,6 +12,7 @@ namespace Proyecto_Intermodular
         float total;
         string text;
         bool isPaid;
+        List<Order> orders;
 
         // Cosntructor por defecto, se llamará cuando creamos un ticket nuevo
         public Ticket()
@@ -20,19 +21,24 @@ namespace Proyecto_Intermodular
             total = 0.0f;
             text = "";
             isPaid = false;
+            orders = new();
         }
 
         // Este constructor se usará si el ticket creado lo sacamos de la base de datos.
-        public Ticket(string id, float total, string ticket, bool isPaid){
-            this.Id = id;
+        public Ticket(string id, float total, string text, bool isPaid, List<Order> orders){
+            this.id = id;
             this.total = total;
-            this.text = ticket;
+            this.text = text;
             this.isPaid = isPaid;
+            this.orders = orders; // ESTE ORDERS LO SACAREMOS DE LA BASE DE DATOS
         }
 
         public string Id { get => id; set => id = value; }
         public float Total { get => total; set => total = value; }
         public bool IsPaid { get => isPaid; set => isPaid = value; }
         public string Text { get => text; set => text = value; }
+
+        public void AddOrder(Order order) => orders.Add(order);
+        public void RemoveOrder(Order order) => orders.Remove(order);
     }
 }
