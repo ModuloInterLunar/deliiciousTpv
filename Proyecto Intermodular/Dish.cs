c8b657c49bfdc2e59a9af12ced136085417b0da4
Alvaro Martinez <alvaromagu2002@gmail.com>
2022-01-05 14:13:57 +0100

    Dish, Drink and Food Class, and ListBox for orders list

---
+++ Proyecto Intermodular/Dish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Intermodular
+{
+    abstract class Dish
+    {
+        private string id;
+        private float price;
+        private string description;
+
+        protected Dish(string id, float price, string description)
+        {
+            this.id = id;
+            this.price = price;
+            this.description = description;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public float Price { get => price; set => price = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
---
+++ Proyecto Intermodular/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Intermodular
+{
+    class Food : Dish
+    {
+        Dictionary<string, float> ingredients;
+        public Food(Dictionary<string, float> ingredients, string id, float price, string description) : base(id, price, description)
+        {
+            this.ingredients = ingredients;
+        }
+
+        public Dictionary<string, float> Ingredients { get => ingredients; set => ingredients = value; }
+    }
+}
