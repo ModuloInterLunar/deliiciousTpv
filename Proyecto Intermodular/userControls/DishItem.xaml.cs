c2a6b46baa8a38882cbf89f8659738542da191ff
Alvaro <alvaromagu2002@gmail.com>
2022-01-29 05:13:53 +0100

    Cambio nombre order de saul por DishItem. Bindings para DishItem puestos.

---
+++ Proyecto Intermodular/userControls/DishItem.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Proyecto_Intermodular.userControls
+{
+    /// <summary>
+    /// Lógica de interacción para Orders.xaml
+    /// </summary>
+    public partial class DishItem : UserControl
+    {
+        public DishItem()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public ImageSource DishImage { set => imgDish.ImageSource = value; get => imgDish.ImageSource; }
+        public string DishName { set => txtBlockDishName.Text = value; get => txtBlockDishName.Text; }
+        public string PriceValue { set => txtBlockPrice.Text = value; get => txtBlockPrice.Text; }
+    }
+}
